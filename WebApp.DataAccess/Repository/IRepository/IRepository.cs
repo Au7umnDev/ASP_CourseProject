4298a323c5de31a3be9606f862ee3cfa99dc1ce2
Gleb Kosilov <allyxan1337@gmail.com>
2023-02-08 22:13:27 +0300

    Add Product functionality

--- WebApp.DataAccess/Repository/IRepository/IRepository.cs
+++ WebApp.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,9 +10,8 @@ namespace WebApp.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         //T - Category, CoverType
-        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
-        IEnumerable<T> GetAll();
-
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        IEnumerable<T> GetAll(string? includeProperties = null);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entities);
---
+++ WebApp.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.DataAccess.Repository.IRepository;
+using WebApp.Models;
+
+namespace WebApp.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+
+            if(objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
+                objFromDb.Category = obj.Category;
+                objFromDb.Author = obj.Author;
+                objFromDb.CoverTypeId = obj.CoverTypeId;
+
+                if(obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+            //_db.Products.Update(obj);
+        }
+    }
+}
